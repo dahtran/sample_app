07a6bff3ae4972692c31b17129ac9e3cd897038f
Darius Tran <dariustran@yahoo.com>
2011-09-14 14:17:17 -0700

    Added a signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,12 +33,10 @@ class User < ActiveRecord::Base
 		encrypted_password == encrypt(submitted_password)
 	end
 	
-	class << self
-		def authenticate(email, submitted_password)
-			user =  find_by_email(email)
-			return nil if user.nil?
-			return user if user.has_password?(submitted_password)
-		end
+	def self.authenticate(email, submitted_password)
+		user =  find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
 	end
 	
 	private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -162,4 +162,4 @@ describe User do
 			end
 		end
 	end
-end
+end	
