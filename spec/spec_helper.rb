063d69eb98c0bb4db753b16386d0354e953bb4a2
Darius Tran <dariustran@yahoo.com>
2011-09-21 10:27:35 -0700

    Editing and updating users

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,13 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 	  def test_sign_in(user)
 		controller.current_user = user
 	  end
+	  
+	  def integration_sign_in(user)
+		visit signin_path
+		fill_in :email,		 :with => user.email
+		fill_in :password,	 :with => user.password
+		click_button
+	  end
 	end
 end
 
