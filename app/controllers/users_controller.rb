3ffa1e97858ee6198e4bb91d17f2bb0cea5bea38
Darius Tran <dariustran@yahoo.com>
2011-09-24 11:38:38 -0700

    Can now destroy users as an admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 	
-	before_filter :authenticate, :only => [:edit, :update, :index]
+	before_filter :authenticate, :only => [:edit, :update, :index, :destroy]
 	before_filter :correct_user, :only => [:edit, :update]
+	before_filter :admin_user,   :only => :destroy
 	
 	def index
 		@users = User.paginate(:page => params[:page])
 		@title = "All Users"
 	end
-
+	
 	def show
 		@user = User.find(params[:id])
 		@title = @user.name
@@ -32,18 +33,21 @@ class UsersController < ApplicationController
 	end
 	  
 	def edit
-		@user = User.find(params[:id])
 		@title = "Edit User"
 	end
 	  
 	def update
-			@user = User.find(params[:id])
 		if @user.update_attributes(params[:user])
 			redirect_to (@user), :flash => { :success => "Profile updated." }
 		else
 			@title = "Edit User"
 			render 'edit'
 		end
+	end
+	
+	def destroy
+		User.find(params[:id]).destroy
+		redirect_to users_path, :flash => { :success => "User destroyed." }
 	end
 	
 	private
@@ -55,6 +59,11 @@ class UsersController < ApplicationController
 		def correct_user
 			@user = User.find(params[:id])
 			redirect_to(root_path) unless current_user?(@user)
+		end
+		
+		def admin_user
+			user = User.find(params[:id])
+			redirect_to(root_path) if (!current_user.admin? || current_user?(user))
 		end
 end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -51,6 +51,21 @@ describe UsersController do
 				response.should have_selector('a.next_page', :href => "/users?page=2",
 												   :content => "Next" )
 			end
+			
+			it "should have a delete link for admins" do
+				@user.toggle!(:admin)
+				other_user = User.all.second
+				get :index
+				response.should have_selector('a', :href => user_path(other_user),
+												   :content => "Delete" )
+			end
+			
+			it "should not have a delete link for non-admins" do
+				other_user = User.all.second
+				get :index
+				response.should_not have_selector('a', :href => user_path(other_user),
+												   :content => "Delete" )
+			end
 		end
 	end
  
@@ -291,6 +306,56 @@ describe UsersController do
 			it "should require matching users for 'user'" do
 				put :update, :id => @user, :user => {}
 				response.should redirect_to(root_path)
+			end
+		end
+	end
+	
+	describe "DELETE 'destroy'" do
+		
+		before(:each) do
+			@user = Factory(:user)
+		end
+		
+		describe "as a non-signed in user" do
+		
+			it "should deny access" do
+				delete :destroy, :id => @user.id
+				response.should redirect_to(signin_path)
+			end
+		end
+		
+		describe "as a non-admin user" do
+			
+			it "should protect the action" do
+				test_sign_in(@user)
+				delete :destroy, :id => @user.id
+				response.should redirect_to(root_path)
+			end
+		end
+		
+		describe "as an admin user" do
+			
+			before(:each) do
+				@admin = Factory(:user, :email => "admin@example.com", :admin => true )
+				test_sign_in(@admin)
+			end
+			
+			it "should destroy the user" do
+				lambda do
+					delete :destroy, :id => @user
+				end.should change(User, :count).by(-1)
+			end
+			
+			it "should redirect to the user's page" do
+				delete :destroy, :id => @user.id
+				flash[:success].should =~ /destroyed/i
+				response.should redirect_to(users_path)
+			end
+			
+			it "should not be able to destroy itself" do
+				lambda do
+					delete :destroy, :id => @admin
+				end.should_not change(User, :count)
 			end
 		end
 	end
